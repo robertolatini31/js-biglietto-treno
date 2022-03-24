70579fd42c6ea2d98ff44876dac7dbd3423b5627
robertolatini31 <robertolatini31@gmail.com>
2022-03-24 13:45:38 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// 0.21 € al km , 20% per i minorenni, 40% per gli over 65
